52588a0fdc7f6d3f96a62c805d865a7331e887db
Joakim Sørensen <joasoe@gmail.com>
2017-10-15 14:02:48 +0200

    ..

--- make_package.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo chown -R root:root package/
-dpkg-deb --build package/
-sudo chown -R landrash:landrash package/
-scp package.deb pi@hassbian.local:~/
-ssh pi@hassbian.local "sudo dpkg -i package.deb && rm package.deb"
